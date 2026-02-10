ce7f27508bdf6af4e5b7f8aea298a8abaf5e0a2e
Francis Brylle <fbrylle12345@gmail.com>
2026-02-10 23:33:15 +0800

    Updated password validation

--- app/schemas/login.py
+++ app/schemas/login.py
@@ -3,5 +3,5 @@ from pydantic import BaseModel, SecretStr, Field
 
 class LoginSchema(BaseModel):
     username : str = Field(min_length=3)
-    password : SecretStr = Field(min_length=3)
+    password : SecretStr = Field(min_length=8, max_length=100)
     
