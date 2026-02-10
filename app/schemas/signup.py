2228def6ff7a8ff8e31bc7aaa74e9ba7f6ef21c6
Francis Brylle <fbrylle12345@gmail.com>
2026-02-10 23:36:10 +0800

    Sync password validation to signup and login

--- app/schemas/signup.py
+++ app/schemas/signup.py
@@ -5,4 +5,4 @@ class RegistrationSchema(BaseModel):
     fname : str = Field(min_length=2, max_length=55)
     lname : str = Field(min_length=2, max_length=55)
     username : str = Field(min_length=3, max_length=20)
-    password : SecretStr
+    password : SecretStr = Field(min_length=8, max_length=100)
