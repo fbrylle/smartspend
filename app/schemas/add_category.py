b10255f6ec59eced798e3de461e062749cd5f723
Francis Brylle <fbrylle12345@gmail.com>
2026-02-13 22:16:49 +0800

    improved validation error handling

--- app/schemas/add_category.py
+++ app/schemas/add_category.py
@@ -1,5 +1,13 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, field_validator
 
 
 class CategorySchema(BaseModel):
-    name : str = Field(min_length=3, max_length=100)
+    name : str = Field(max_length=100)
+
+
+    @field_validator('name', mode='after')
+    @classmethod
+    def name_length(cls, v):
+        if len(v) <= 2:
+            raise ValueError('Category should be atleast 3 characters long')
+        return v
