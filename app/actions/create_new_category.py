a27be7ac48ce6549998bd9602989c3f9f1639bed
Francis Brylle <fbrylle12345@gmail.com>
2026-02-13 22:18:00 +0800

    modified add category helper

--- app/actions/create_new_category.py
+++ app/actions/create_new_category.py
@@ -1,12 +1,15 @@
 from app.extensions import db
 from app.models import Category
 from app.schemas import CategorySchema
+from pydantic import ValidationError
 
 
 def create_new_category(current_user, form_data):
     """ Create new category and Save to db """
-
-    valid_data = CategorySchema(**form_data)
+    try:
+        valid_data = CategorySchema(**form_data)
+    except ValidationError as e:
+        return False, f"{e.errors()[0]['msg']}"
 
     try:
         new_category = Category(
