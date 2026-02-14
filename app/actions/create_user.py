f600795fc0c4e85d4d2e84b021c017c86844637f
Francis Brylle <fbrylle12345@gmail.com>
2026-02-15 01:54:49 +0800

    move pydantic validation to create user action

--- app/actions/create_user.py
+++ app/actions/create_user.py
@@ -1,10 +1,16 @@
 from app.extensions import db
 from app.models import Users
 from pydantic import ValidationError
+from app.schemas import RegistrationSchema
 
 
 def create_new_user(valid_data):
     """ Save new user to db """
+
+    try:
+        valid_data = RegistrationSchema(**valid_data)
+    except ValidationError as e:
+        return False, f"{e.errors()[0]['msg']}"
 
     try:
         new_user = Users( 
