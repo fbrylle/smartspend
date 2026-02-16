24c7a672500bd068052657299ae49c00de0b9d29
Francis Brylle <fbrylle12345@gmail.com>
2026-02-17 00:14:47 +0800

    modified login validation

--- app/schemas/login.py
+++ app/schemas/login.py
@@ -1,7 +1,23 @@
-from pydantic import BaseModel, SecretStr, Field
+from pydantic import BaseModel, SecretStr, Field, field_validator
 
 
 class LoginSchema(BaseModel):
-    username : str = Field(min_length=3)
-    password : SecretStr = Field(min_length=8, max_length=100)
+    username : str = Field(max_length=100)
+    password : SecretStr = Field(max_length=100)
     
+
+    @field_validator('username', mode='after')
+    @classmethod
+    def check_username_input(cls, username):
+        if len(username) <= 2:
+            raise ValueError('Incorrect email or password')
+        return username
+    
+
+    @field_validator('password', mode='after')
+    @classmethod
+    def password_input(cls, password: SecretStr):
+        if len(password.get_secret_value()) <= 8:
+            raise ValueError('Incorrect email or password')
+        return password
+        
