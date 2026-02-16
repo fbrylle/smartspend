17ecee09b082ef3eb26b5db22ef5cd58363298e0
Francis Brylle <fbrylle12345@gmail.com>
2026-02-17 00:16:32 +0800

    modifie authenticate user helper

--- app/actions/authenticate_user.py
+++ app/actions/authenticate_user.py
@@ -2,10 +2,11 @@ from app.extensions import db
 from app.models import Users
 
 
-def authenticate_user(valid_data):
+
+def authenticate_user(form_data):
     """Authenticate user credential"""
-
-    user = Users.query.filter_by(username=valid_data.username).first()
-
+    
+    user = Users.query.filter_by(username=form_data.username).first()
     return user
+
 
